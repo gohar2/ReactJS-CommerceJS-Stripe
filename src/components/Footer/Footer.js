aa2cbba9b61768ea4843ac04af19138d39f4fbd3
gohar <gohar@gmail.com>
2022-02-28 13:13:03 +0500

    code pushed to git

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
+import logo from '../../assets/circles.png';
+
+const Footer = () => {
+  return (
+    <MDBFooter color='unique-color-dark' className='font-small pt-4 mt-4'>
+      <MDBContainer className='text-center text-md-left'>
+        <MDBRow className='text-center text-md-left mt-3 pb-3'>
+          <MDBCol md='3' lg='3' xl='4' className='mx-auto mt-3'>
+            <h6 className='text-uppercase mb-4 font-weight-bold'>
+              <img src={logo} alt='Book Store App' height='50px' />
+              <strong>Get-Book</strong>
+            </h6>
+            <p>
+              Get-Book is an online React web application where the customer can
+              purchase books online. Through this book store the users can
+              search for a book by its title and later can add to the shopping
+              cart and finally purchase using credit card transaction.
+            </p>
+          </MDBCol>
+          <hr className='w-100 clearfix d-md-none' />
+          <MDBCol md='2' lg='2' xl='2' className='mx-auto mt-3'>
+            <h6 className='text-uppercase mb-4 font-weight-bold'>
+              <strong>Products</strong>
+            </h6>
+            <p>
+              <a href='#'>Get-Book</a>
+            </p>
+          </MDBCol>
+
+          <hr className='w-100 clearfix d-md-none' />
+          <MDBCol md='4' lg='3' xl='3' className='mx-auto mt-3'>
+            <h6 className='text-uppercase mb-4 font-weight-bold'>
+              <strong>Contact</strong>
+            </h6>
+            <p>
+              <i className='fa fa-phone mr-3' /> +91 1234334321
+            </p>
+            <p>
+              <i className='fa fa-home mr-3' /> city, state
+            </p>
+          </MDBCol>
+        </MDBRow>
+        <hr />
+        <MDBRow className='d-flex align-items-center'>
+          <MDBCol md='8' lg='8'>
+            <p className='text-center text-md-left grey-text'>
+              &copy; {new Date().getFullYear()} Made by
+              <a href='https://github.com/gohar2'> Gohar </a>
+            </p>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </MDBFooter>
+  );
+};
+
+export default Footer;
---
+++ src/components/Products/Product/styles.js
@@ -0,0 +1,39 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  root: {
+    maxWidth: '100%',
+    background: 'linear-gradient(45deg, #D9D9D9 30%, #E6E6E6 90%)'
+  },
+  media: {
+    height: 0,
+    paddingTop: '100%',
+    '&:hover': {
+      backgroundColor: '#2a344a',
+      boxShadow: 'none'
+    }
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  productName: {
+    fontSize: '15px !important'
+  },
+  button: {
+    background: '#1C2331',
+    color: 'white',
+    width: '100%',
+    height: '40px',
+
+    '&:hover': {
+      backgroundColor: '#2a344a',
+      boxShadow: 'none'
+    }
+  }
+}));
